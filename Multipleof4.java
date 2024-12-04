24ab1d7a7b3c167baee6c67329d9dcaeca179764
chaitud2006 <cc2214574@gmail.com>
2024-12-04 00:16:35 -0800

    Add files via upload

---
+++ Multipleof4.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Multipleof4
+{
+    public static void main(String args[])
+    {
+        int a=56;
+        if((a&3)==0)
+        {
+            System.out.println("Multiple of 4");
+        }
+        else
+        {
+            System.out.println("Not a multiple of a");
+        }
+    }
+}
